f7e21b68c25c554fad6c8b4aebe8bcc80583d648
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-03-25 22:55:47 -0400

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -15,4 +15,4 @@ async fn main() -> Result<(), std::io::Error> {
     .bind("127.0.0.1:8000")?
     .run()
     .await
-} 
+}
