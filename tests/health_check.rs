9d2799199fdeba89796749a5e7ea54de5ca3b970
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-03-31 21:21:49 -0400

    first integration test for /health_check

---
+++ tests/health_check.rs
@@ -0,0 +1,30 @@
+
+// tokio::test is the equivalent of tokio::main + test attribute
+// You can expand the code generated with 
+// `cargo expand --test health_check`
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+    // setup reqwest to make http requests
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind to address");
+
+    // non-binding let because the server is spawned as a background task
+    // We don't need the handle in scope, so we don't bind it to a variable name. 
+    // tokio handles cleanup when the runtime is dropped
+    let _ = tokio::spawn(server);
+}
