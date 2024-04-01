afd4c4f9de221db61904f4588d1d48e26d5b7a3b
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-03-31 21:34:45 -0400

    polished tests to use any available port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,17 +1,18 @@
+use std::net::TcpListener;
 
 // tokio::test is the equivalent of tokio::main + test attribute
-// You can expand the code generated with 
+// You can expand the code generated with
 // `cargo expand --test health_check`
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // setup reqwest to make http requests
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -20,11 +21,15 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind to address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to an available port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind to address");
 
     // non-binding let because the server is spawned as a background task
-    // We don't need the handle in scope, so we don't bind it to a variable name. 
+    // We don't need the handle in scope, so we don't bind it to a variable name.
     // tokio handles cleanup when the runtime is dropped
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
